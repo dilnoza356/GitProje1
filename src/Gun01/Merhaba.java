c4a509cd3de0d21420f2ffef69e4c381ab99133d
Dilnoza_İlmuradova <159903212+dilnoza356@users.noreply.github.com>
2024-02-15 01:18:33 +0300

    task5

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,15 +4,17 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
         for (int i = 0; i <4 ; i++) {
-            System.out.println("merhab 4");
+            System.out.println("merhaba 4");
 
         }
-        System.out.println("merhab 3");
+        System.out.println("merhaba 3");
 
         for (int  i= 0;  i < 5; i++) {
             System.out.println("merhaba");
 
             System.out.println("merhaba6");
+
+            System.out.println("Merhaba dunya");
 
         }
 
